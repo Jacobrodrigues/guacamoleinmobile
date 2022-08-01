0c7d94c47128b082f8e47e539a3ad66722c60557
Jacob Rodrigues <jacob.neto@hotmail.com>
2022-08-01 17:35:48 -0300

    DART002.2 - atualizacao na introducao de funcoes - criacao de funcoes com return

--- funcoes/introd-funcao.dart
+++ funcoes/introd-funcao.dart
@@ -1,1 +1,17 @@
+void main() {
+  saudacoes();
 
+  String data = agora();
+  print(data);
+}
+
+void saudacoes() {
+  print('Saudações do Jacob');
+  print('seja bem-vindo!');
+  print('AGORA : ${agora()}');
+}
+
+String agora() {
+  DateTime agora = DateTime.now();
+  return agora.toString();
+}
