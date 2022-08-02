326db6801331aadb6165f83956f0e58ac5ba2bbb
Jacob Rodrigues <jacob.neto@hotmail.com>
2022-08-01 23:35:54 -0300

    DART003- colecoes

---
+++ colecao/listas.dart
@@ -0,0 +1,17 @@
+void main() {
+  List<String> nomes = [
+    'Daniel',
+    'Cliolfi',
+    'Henrique',
+  ];
+
+  print(nomes);
+
+  List<int> idades = [15, 32, 56, 78];
+
+  print(idades.length);
+  print(idades.last);
+  print(idades.first);
+  print(idades[2]);
+  print(idades[1]);
+}
---
+++ colecao/variaveis-aleatorias.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  var nome = 'Daniel';
+  nome = 'Cliolfi';
+  // nome = 3
+
+  var idade = 40;
+  idade++;
+
+  dynamic variavel = 'Daniel';
+  variavel = 3;
+  variavel = true;
+
+  num numero = 1.363737;
+}
+
+void funcao(num numero) {}
