7ea1a4bb98d64b747c7cf7464143fd1871c2e465
Jacob Rodrigues <jacob.neto@hotmail.com>
2022-08-03 09:49:02 -0300

    DART002 - listas em dart

--- colecao/listas.dart
+++ colecao/listas.dart
@@ -8,10 +8,21 @@ void main() {
   print(nomes);
 
   List<int> idades = [15, 32, 56, 78];
+  List<int> idade_novas = [123, 142, 9183];
+  //List<int> compras = [];
+  idades.add(4);
+  idades.add(466);
 
+  idades.addAll(idade_novas);
   print(idades.length);
   print(idades.last);
   print(idades.first);
+  print(idades.contains(15));
+  print(idades.indexOf(3));
+  print(idades.remove(4));
+  print(idades.removeAt(4));
+  idades.shuffle();
+  idades.clear();
   print(idades[2]);
   print(idades[1]);
 }
